47684912074be7aaace7aa16a86ddd9be9d39a83
darksparro123 <darksparro156@gmail.com>
2021-02-07 10:05:28 +0530

    sgow registered customers done

--- scripts/customers.js
+++ scripts/customers.js
@@ -8,52 +8,58 @@ var firebaseConfig = {
     storageBucket: "delivoblackgenpvtltd.appspot.com",
     messagingSenderId: "776352742517",
     appId: "1:776352742517:web:d089b32ad13f6e6ad18c3c",
-    measurementId: "G-QER3M9EHP7"
+    measurementId: "G-QER3M9EHP7",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 const auth = firebase.auth();
 const db = firebase.firestore();
-
+db.settings({
+    timestampsInSnapshots: true,
+});
 const customerDetailsList = document.querySelector("#customer-details-list");
 
-db.collection('Customer').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
-
-        getCustomerCollection(doc.id);
-    });
-})
+db.collection("Customer")
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            getCustomerCollection(doc.id);
+        });
+    })
+    .catch(function(error) {});
 
 // get customer details collection
 function getCustomerCollection(docId) {
     console.log("called");
-    db.collection("Customer").doc(docId).collection("Customer's Details").get()
+    db.collection("Customer")
+        .doc(docId)
+        .collection("Customer's Details")
+        .get()
         .then((snapshot) => {
-            snapshot.docs.forEach(doc => {
-                console.log(doc.data()["Custumer's Name"])
-                console.log(doc.data()["Custumer's Address"])
-                console.log(doc.data()["Custumer's Mobile"])
+            snapshot.docs.forEach((doc) => {
+                console.log(doc.data()["Custumer's Name"]);
+                console.log(doc.data()["Custumer's Address"]);
+                console.log(doc.data()["Custumer's Mobile Number"]);
                 console.log(doc.id);
-                renderItems(doc)
-            })
-        })
+                renderItems(doc);
+            });
+        });
 }
 
 function renderItems(doc) {
-    let tableRow = document.createElement('tr');
-    let cName = document.createElement('td');
-    let cAddress = document.createElement('td');
+    let tableRow = document.createElement("tr");
+    let cName = document.createElement("td");
+    let cAddress = document.createElement("td");
     let cMobile = document.createElement("td");
     let cEmail = document.createElement("td");
     let ordersButton = document.createElement("button");
 
-    ordersButton.textContent = "orders"
+    ordersButton.textContent = "orders";
     cName.textContent = doc.data()["Custumer's Name"];
     cAddress.textContent = doc.data()["Custumer's Address"];
-    cMobile.textContent = doc.data()["Custumer's Mobile"];
+    cMobile.textContent = doc.data()["Custumer's Mobile Number"];
     cEmail.textContent = doc.data()["Custumer's Email"];
-
 
     tableRow.setAttribute("doc-id", doc.id);
 
@@ -63,31 +69,32 @@ function renderItems(doc) {
     tableRow.appendChild(cEmail);
     tableRow.appendChild(ordersButton);
 
-    ordersButton.setAttribute("id", "orders")
+    ordersButton.setAttribute("id", "orders");
 
     customerDetailsList.appendChild(tableRow);
-    document.getElementById("orders").onclick = function() { getCustomersOrders() };
+    document.getElementById("orders").onclick = function() {
+        getCustomersOrders();
+    };
 }
-
 
 function getCustomersOrders() {
-    db.collection("orders").get().then((
-        snapshot) => {
-        snapshot.docs.forEach(doc => {
-            db.collection("order_status").get().then(snap => {
-                snap.docs.forEach(document => {
-                    console.log("docs id is " + document.id)
-                    var n = document.id.localeCompare(doc.data()["order_status_id"]);
-                    if (n == 0) {
-                        console.log(doc.data());
-                    }
-                })
-
-            })
-        })
-    })
+    db.collection("orders")
+        .get()
+        .then((snapshot) => {
+            snapshot.docs.forEach((doc) => {
+                db.collection("order_status")
+                    .get()
+                    .then((snap) => {
+                        snap.docs.forEach((document) => {
+                            console.log("docs id is " + document.id);
+                            var n = document.id.localeCompare(doc.data()["order_status_id"]);
+                            if (n == 0) {
+                                console.log(doc.data());
+                            }
+                        });
+                    });
+            });
+        });
 }
 
-function renderOrders(orderStatusId) {
-
-}
+function renderOrders(orderStatusId) {}
--- scripts/riderRequests.js
+++ scripts/riderRequests.js
@@ -6,7 +6,7 @@ var firebaseConfig = {
     storageBucket: "delivoblackgenpvtltd.appspot.com",
     messagingSenderId: "776352742517",
     appId: "1:776352742517:web:d089b32ad13f6e6ad18c3c",
-    measurementId: "G-QER3M9EHP7"
+    measurementId: "G-QER3M9EHP7",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -14,49 +14,48 @@ firebase.analytics();
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-
 function getRiderData() {
-    db
-        .collection("Riders").where("admin permission", "==", false)
+    db.collection("Riders")
+        .where("admin permission", "==", false)
         .get()
         .then(function(querySnapshot) {
             querySnapshot.docs.forEach(function(doc) {
                 console.log(doc.id);
-                getRiderdetails(doc.id)
+                getRiderdetails(doc.id);
             });
         })
         .catch(function(error) {
             console.log("get data failed" + error);
-        })
+        });
 }
 
 getRiderData();
 
 function giveRiderPermission(riderId) {
-    db.collection("Riders").doc(riderId)
+    db.collection("Riders")
+        .doc(riderId)
 
     .update({
-            "admin permission": true
+            "admin permission": true,
         })
         .then(function(doc) {
             console.log("update succussfully");
         })
         .catch(function(error) {
             console.log("update failed" + error);
-        })
+        });
 }
 
 function getRiderdetails(docId) {
     console.log("called");
-    db
-        .collection("Riders").doc(docId).collection("Riders's Details")
+    db.collection("Riders")
+        .doc(docId)
+        .collection("Riders's Details")
         .get()
         .then(function(querySnapshot) {
             querySnapshot.forEach(function(doc) {
-                console.log(doc.data())
+                console.log(doc.data());
             });
         })
-        .catch(function(error) {
-
-        })
+        .catch(function(error) {});
 }
